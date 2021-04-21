74ea7134b5d344cdb0637ea9b5ae2a0a0eabe05b
testaiml <82972939+testaiml@users.noreply.github.com>
2021-04-22 03:34:25 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1XTuWAvdDSqxsduCDpOEzgm2RqDNsA-GQ/view?usp=sharing'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1g6o_k48kdJFyCT0TGPb8jOpDZjIOleDC'
 export_file_name = 'export.pkl'
 
 classes = ['bulbasaur','charmander','pikachu','squirtle']
